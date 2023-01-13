b80c463fd62b53b9958d5594cabda96aa0691061
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2023-01-12 18:34:49 -0800

    added relevant data as csv files and changed to dictionary solution

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 import nfl_data_py as nfl
 import time
 import os
+from tqdm import tqdm
 
 def main():
     # Look into how nfl_data_py works   
     start_time = time.time()
 
-    # Finding the players with the most passing yards and rushing yards in the last decade
+    # Finding the players with the most passing yards in the last decade
     
     decade = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
   
     decade_data = nfl.import_seasonal_data(decade)
     df = decade_data.reset_index()
 
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    max_passing_yards = 0
-    max_passer_id = ''
-    max_rushing_yards = 0
-    max_rusher_id = ''
-
-    for index, row in df.iterrows():
-        if row['passing_yards'] > max_passing_yards:
-            max_passing_yards = row['passing_yards']
-            max_passer_id = row['player_id']
-        if row['rushing_yards'] > max_rushing_yards:
-            max_rushing_yards = row['rushing_yards']
-            max_rusher_id = row['player_id']
-
     play = nfl.import_rosters(decade)
     dfp = play.reset_index()
-    f1 = 0
-    f2 = 0
-    for index, row in dfp.iterrows():
-        if row['player_id'] == max_passer_id and f1 == 0:
-            print(row['player_name'], row['team'],  f'{max_passing_yards} passing yards')
-            f1 += 1
-        if row['player_id'] == max_rusher_id and f2 == 0:
-            print(row['player_name'], row['team'], f'{max_rushing_yards} rushing yards')
-            f2 += 1
-        
-    print("--- %s seconds ---" % (time.time() - start_time))
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    df_dict = df.to_dict('records')
+    dfp_dict = dfp.to_dict('records')
+    
+    id_passing_yards = []
+    passing_yards = []
+    player_name = ''
+
+    for i in tqdm(range(len(df_dict))):
+        id_passing_yards.append([df_dict[i]['player_id'], df_dict[i]['passing_yards']])
+        passing_yards.append(df_dict[i]['passing_yards'])
+
+    maxyds = max(passing_yards)
+    maxid = id_passing_yards[passing_yards.index(maxyds)][0]
+
+    for i in tqdm(range(len(dfp_dict))):
+        if dfp_dict[i]['player_id'] == maxid:
+            player_name = dfp_dict[i]['player_name']
+            break
+
+    print(player_name, maxyds)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     return 0
 
